a4482f4589b52856d2902d48ac197bb00ac33672
Ryan Pennington <rypenn27@yahoo.com>
2021-01-03 12:29:08 -0500

    sequelize models defined, init db via sequelize sync

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 // Connect to DB
 const { Client } = require("pg");
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 
 const DB_NAME = "linkerator";
 const DB_URL =
@@ -9,11 +9,79 @@ const DB_URL =
 const client = new Client(DB_URL);
 const sequelize = new Sequelize(DB_URL); // Example for postgres
 
+const Link = sequelize.define(
+  "Link",
+  {
+    // Model attributes are defined here
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    clickCount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    comment: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    creationDate: {
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.NOW,
+    },
+  },
+  {
+    // Other model options go here
+  }
+);
+
+const Tag = sequelize.define(
+  "Tag",
+  {
+    // Model attributes are defined here
+    tag: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    // Other model options go here
+  }
+);
+
+const LinkTags = sequelize.define("LinkTags", {
+  LinkId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Link,
+      key: "id",
+    },
+  },
+  TagId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Tag,
+      key: "id",
+    },
+  },
+});
+
+Link.belongsToMany(Tag, { through: LinkTags });
+Tag.belongsToMany(Link, { through: LinkTags });
+
+const models = {
+  Link,
+  Tag,
+  LinkTags,
+};
+
 // database methods
 
 // export
 module.exports = {
   client,
   sequelize,
+  models,
   // db methods
 };
